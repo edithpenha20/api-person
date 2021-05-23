f0d4e2f879f588db984a0d42c42ee62d7b9aa796
Karmaicom <karmaicom@gmail.com>
2021-05-23 11:58:20 -0300

    Ajustes

--- src/main/java/one/digitalinnovation/personapi/exception/PersonNotFoundException.java
+++ src/main/java/one/digitalinnovation/personapi/exception/PersonNotFoundException.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class PersonNotFoundException extends Exception {
-    public PersonNotFoundException(Long id) {
+	private static final long serialVersionUID = 28495874871067123L;
+
+	public PersonNotFoundException(Long id) {
         super("Person not found with ID" + id);
     }
 }
