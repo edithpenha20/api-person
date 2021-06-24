da651036beb8a96c4335134b64ba65c3b687e153
Edith Penha <edithpenha@gmail.com>
2021-06-24 16:08:29 -0400

    Developed delete by Id operation

--- src/main/java/one/digitalinnovation/personapi/entity/Person.java
+++ src/main/java/one/digitalinnovation/personapi/entity/Person.java
@@ -1,18 +1,25 @@
 package one.digitalinnovation.personapi.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
 @Entity
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Person {
--- src/main/java/one/digitalinnovation/personapi/exception/PersonNotFoundException.java
+++ src/main/java/one/digitalinnovation/personapi/exception/PersonNotFoundException.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class PersonNotFoundException extends Exception {
+    private static final long serialVersionUID = 28495874871067123L;
+
     public PersonNotFoundException(Long id) {
         super("Person not found with ID" + id);
     }
